b5284def1e10b80558ff92f9335ecddd109834f0
JarvisXxX <eldrizzy@hotmail.com>
2018-06-10 10:21:21 -0600

    Configurando la conexion a la base de datos PersonasDb, ConnectionString,  agregando EntityFramework al proyecto, Creando la primera entidad Persona.

---
+++ RegistroEjemplo/Entidades/Persona.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RegistroEjemplo.Entidades
+{
+    public class Persona
+    {
+        [Key]
+        public int PersonaId { get; set; }
+        
+        public String Nombres { get; set; }
+        public String Cedula { get; set;}
+        public String Telefono { get; set;}
+        public String Direccion { get; set;}
+
+        public Persona()
+        {
+            PersonaId = 0;
+            Nombres = String.Empty;
+            Cedula = String.Empty;
+            Telefono = String.Empty;
+            Direccion = String.Empty;
+
+
+        }
+
+
+    }
+}
